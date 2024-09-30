512381b9f9088083696da04ab6d3a68e1a79d69f
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2024-09-30 22:29:33 +0200

    Locale core package.

--- imogrify/core/locale/include/imfy/locale.hpp
+++ imogrify/core/locale/include/imfy/locale.hpp
@@ -3,12 +3,12 @@
  * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-#include <imfy/locale.hpp>
+#pragma once
 
-#include <cstdlib>
-
-int main()
+namespace imfy::locale
 {
-	imfy::locale::initialize();
-	return EXIT_SUCCESS;
+
+/** Use UTF-8 as the default encoding for Boost.Filesystem and the global C++ locale. */
+void initialize();
+
 }
---
+++ imogrify/core/locale/locale.cpp
@@ -0,0 +1,20 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include "imfy/locale.hpp"
+
+#include <boost/nowide/filesystem.hpp>
+
+#include <locale>
+
+namespace imfy::locale
+{
+
+void initialize()
+{
+	std::locale::global(boost::nowide::nowide_filesystem());
+}
+
+} // namespace imfy::locale
